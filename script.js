a1cf6b049eef64dad3ed476fd7ec6b72cdb4dcf9
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-10-03 20:57:48 +0200

    Resolucao exercicios da tarde

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const addFrutaNaLista = () => {
+  for(let i=0; i < 3; i++){
+    const itemLista = document.getElementById(`fruta-${[i+1]}`)
+    // console.log(itemLista)
+    itemLista.innerHTML = frutas[i]
+  }
+}
+
+addFrutaNaLista()
+
+const input = document.getElementById("texto")
+console.log(input)
+const addNovaFruta = () => {
+  const areaUl = document.getElementById("lista-de-frutas")
+  // console.log(input.value)
+  areaUl.innerHTML += `<li>${input.value}</li>`
+  input.value = ""
+}
+
